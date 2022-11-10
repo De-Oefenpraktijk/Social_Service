a3aa7266c84658be8c8f37003852ec63a0010f99
Rayco Haex <raycoh1299@gmail.com>
2022-11-10 10:23:52 +0100

    added boilerplate code

---
+++ OEF-Social_Service/OEF-Social_Service/EventBus.Consumers/ProfileUpdatedConsumer.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using MassTransit.Mediator;
+using MassTransit;
+using EventBus.Messages.Events;
+
+namespace OEF_Social_Service.EventBus.Consumer
+{
+    public class ProfileUpdatedConsumer : IConsumer<ProfileUpdatedEvent>
+    {
+        private readonly IMapper _mapper;
+        private readonly IMediator _mediator;
+        private readonly ILogger<ProfileUpdatedConsumer> _logger;
+
+        public ProfileUpdatedConsumer(IMapper mapper, IMediator mediator, ILogger<ProfileUpdatedConsumer> logger)
+        {
+            _mapper = mapper;
+            _mediator = mediator;
+            _logger = logger;
+        }
+
+        public async Task Consume(ConsumeContext<ProfileUpdatedEvent> context)
+        {
+            //var entity = _mapper.Map<KweetEntity>(context.Message);
+
+        }
+    }
+}
---
+++ OEF-Social_Service/OEF-Social_Service/EventBus.Messages/Events/ProfileUpdatedEvent.cs
@@ -0,0 +1,26 @@
+﻿namespace EventBus.Messages.Events
+{
+    public class ProfileUpdatedEvent: IntegrationBaseEvent
+    {
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        // used as unique identifier
+        public string Username { get; set; } = null!;
+
+        public string? EmailAddress { get; set; }
+
+        public string? Password { get; set; }
+
+        public DateTime EnrollmentDate { get; set; }
+
+        public string? Role { get; set; }
+
+        public string? Institutions { get; set; }
+
+        public string? Themes { get; set; }
+
+        public string? ResidencePlace { get; set; }
+    }
+}

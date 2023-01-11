1000a9d6d1da6c61b140d0894ac1308056d5516e
Emirhan Demirci <emirhan.demirci@feironsoft.com>
2023-01-11 10:59:45 +0100

    Id added in the consumer

--- OEF-Social_Service/OEF-Social_Service/EventBus.Messages/Events/ProfileUpdatedEvent.cs
+++ OEF-Social_Service/OEF-Social_Service/EventBus.Messages/Events/ProfileUpdatedEvent.cs
@@ -2,6 +2,7 @@
 {
     public class ProfileUpdatedEvent: IntegrationBaseEvent
     {
+        public string? Id { get; set; }
         public string? FirstName { get; set; }
 
         public string? LastName { get; set; }
